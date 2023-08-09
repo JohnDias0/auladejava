2c4bb53db4ea57b2016024f7d55b8e03bf97dd06
JohnDias0 <dias44520@gmail.com>
2023-08-08 21:19:28 -0300

    finalizei a classe do piloto em Piloto.java e criei a nova classe: Carro.java

--- Piloto.java
+++ Piloto.java
@@ -7,4 +7,18 @@ public class Piloto extends Pessoa {
         super(Ref, height, weight, energy);
         this.team = team;
 }
+    void corrida(){
+        System.out.println(super.name + " da a partida no carro e inicia a corrida com uma arrancada magnifica digna de um filme do Velozes e Furiosos.");
+        this.stamina -= 1; 
+    }
+    void acelerar(){
+        if(this.stamina < 4 && this.hability <= 10){
+            System.out.println("O piloto " + super.name + " inicia uma investida arriscada e tenta conquistar o seu lugar na corrida");
+            System.out.println("Mas acaba perdendo o controle do carro  e perde a corrida");
+        };
+        if(this.stamina >= 4 && this.hability > 10){
+            System.out.println("O piloto " + super.name + " inicia uma investida  arriscada e tenta conquistar o seu lugar na corrida");
+            System.out.println("Inacreditável!!! Ele ascende com maestria ao primeiro lugar. É o  graaanddeee, é o maior dos maiores, é o extraordinário " + super.name + "!!!!!!!!!!");
+        };
+    }
 }
